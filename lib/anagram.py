b6fec9188ec9306a7ef5b1bee553a9136c8feeca
braxtonshaffie <braxtonshaffie@gmail.com>
2023-05-17 23:50:21 +0300

    done with assignment

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,14 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def find_anagrams(self, words):
+        anagrams = []
+        for w in words:
+            if sorted(w.lower()) == sorted(self.word.lower()) and w.lower() != self.word.lower():
+                anagrams.append(w)
+        return anagrams
+
+    def match(self, words):
+        return self.find_anagrams(words)
